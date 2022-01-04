0f07a420858f3e4f90b1374fb73221bfd7c7e915
Ali Hasan <ahb7671@gmail.com>
2022-01-04 09:43:14 +0500

    sweet: import devicesettings
    Co-authored-by: Daniel <daniel@danielml.dev>
    Co-authored-by: Abhishek Aggarwal <warabhishek@gmail.com>
    Co-authored-by: Peter Cai <peter@typeblog.net>
    Co-authored-by: Joey Bevilacqua <bevilacquajoey@gmail.com>
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- device.mk
+++ device.mk
@@ -53,8 +53,8 @@ PRODUCT_PACKAGES += \
     Snap
 
 # Device Settings
-#PRODUCT_PACKAGES += \
-    XiaomiParts
+PRODUCT_PACKAGES += \
+    DeviceSettings
 
 #PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-parts.xml
